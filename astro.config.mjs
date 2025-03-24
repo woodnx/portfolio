b9126f769d597878d65a3fc3205ccef26f8de02f
woodnext <shun20020611@yahoo.co.jp>
2025-03-24 10:37:44 +0900

    Installed `Expressive Code`

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,27 @@ import { defineConfig } from 'astro/config';
 // 3rd-party plugin
 import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
+import expressiveCode from 'astro-expressive-code';
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 // user defined plugin
 import { remarkModifiedTime } from './src/scripts/remark-modified-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [
+    icon(), 
+    expressiveCode({
+      plugins: [
+        pluginLineNumbers(),
+      ],
+      styleOverrides: {
+        frames: {
+          shadowColor: '#FFF',
+        },
+      },
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()]
