236182f58e660f709d44152f09674d473417bcd0
woodnext <shun20020611@yahoo.co.jp>
2025-04-01 19:24:37 +0900

    Make auto adding `publishedAt` Fixes #32

--- src/scripts/remark-modified-time.mjs
+++ src/scripts/remark-modified-time.mjs
@@ -3,7 +3,12 @@ import { execSync } from "child_process";
 export function remarkModifiedTime() {
   return function (tree, file) {
     const filepath = file.history[0];
-    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
-    file.data.astro.frontmatter.lastModified = result.toString();
+    const result = execSync(`git log --pretty="format:%cI" "${filepath}"`);
+    const times = result.toString().split('\r');
+    file.data.astro.frontmatter.publishedAt = times[-1];
+
+    if (times.length > 1) {
+      file.data.astro.frontmatter.lastModified = times[0];
+    }
   };
 }
