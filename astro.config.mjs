cae0fbd4bb1dd6496a83cecdcc340bef6cae0e9e
woodnext <shun20020611@yahoo.co.jp>
2026-06-19 17:11:26 +0900

    依存関係をアップデート

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,48 +1,36 @@
 // @ts-check
-import { defineConfig, passthroughImageService } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import vue from '@astrojs/vue';
+import { defineConfig, passthroughImageService } from "astro/config";
+import mdx from "@astrojs/mdx";
+import vue from "@astrojs/vue";
 
 // 3rd-party plugin
-import icon from 'astro-icon';
-import tailwindcss from '@tailwindcss/vite';
-import expressiveCode from 'astro-expressive-code';
-import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
-import remarkLinkCard from 'remark-link-card';
-import { remarkAlert } from 'remark-github-blockquote-alert';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-import swup from '@swup/astro';
+import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
+import remarkLinkCard from "remark-link-card-plus";
+import { unified } from "@astrojs/markdown-remark";
+import { remarkAlert } from "remark-github-blockquote-alert";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import swup from "@swup/astro";
 
 // user defined plugin
-import { remarkModifiedTime } from './src/scripts/remark-modified-time.mjs';
-
+import { remarkModifiedTime } from "./src/scripts/remark-modified-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: passthroughImageService(),
+    remotePatterns: [
+      { protocol: "http", hostname: "img.youtube.com" },
+      { protocol: "https", hostname: "img.youtube.com" },
+    ],
   },
   integrations: [
-    icon(), 
-    expressiveCode({
-      plugins: [
-        pluginLineNumbers(),
-      ],
-      styleOverrides: {
-        frames: {
-          shadowColor: '#FFF',
-        },
-      },
-    }), 
+    icon(),
     mdx({
-      remarkPlugins: [
-        remarkLinkCard,
-        remarkAlert,
-        remarkMath,
-      ],
+      remarkPlugins: [remarkLinkCard, remarkAlert, remarkMath],
       rehypePlugins: [rehypeKatex],
-    }), 
+    }),
     vue(),
     swup({
       containers: ["main", "aside"],
@@ -50,14 +38,16 @@ export default defineConfig({
   ],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   markdown: {
-    remarkPlugins: [remarkModifiedTime],
-    remarkRehype: {
-      footnoteLabel: '脚注',
-      footnoteLabelTagName: 'h1',
-    },
+    processor: unified({
+      remarkPlugins: [remarkModifiedTime],
+      remarkRehype: {
+        footnoteLabel: "脚注",
+        footnoteLabelTagName: "h1",
+      },
+    }),
   },
 });
